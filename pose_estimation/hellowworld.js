07b1f1167c72b0f412d882b31aeac535fca16d8a
EvgeniiMaslov <sqrt.evmaslov@gmail.com>
2020-08-10 15:26:14 +0300

    upload files via update

---
+++ pose_estimation/hellowworld.js
@@ -0,0 +1,11 @@
+let app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Привет, Vue.js!'
+    },
+    methods: {
+      reverseMessage: function() {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
